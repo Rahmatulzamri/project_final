b4436efbef6c93796a8e8090e6c7252900222a0a
fadilalfarizi <fadilalfarizi576@gmail.com>
2024-06-06 06:27:43 -0800

    ganti harga test dashboard

--- rentcar-app/routes/web.php
+++ rentcar-app/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view ('layout.master');
-// });
+Route::get('/', function () {
+    return view ('layout.master');
+});
 Route::get('/dashboard', [HomeController::class, 'index']);
