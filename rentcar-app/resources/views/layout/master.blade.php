9d8729438bad1f059f8b4e38a83a0881230677f5
M. Farel Asyrofi, A.Md.Kom <m.farelasyrofi@gmail.com>
2024-06-09 22:01:44 +0700

    Auth: Comment Guest

--- rentcar-app/resources/views/layout/master.blade.php
+++ rentcar-app/resources/views/layout/master.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>
-          Rent Car App
-        </title>
+    <title>
+        Rent Car App
+    </title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{ asset('admin/template/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/template/assets/vendors/css/vendor.bundle.base.css') }}">
@@ -20,56 +21,62 @@
     <link rel="stylesheet" href="{{ asset('admin/template/assets/css/style.css') }}">
     <!-- End layout styles -->
     <link rel="shortcut icon" href="{{ asset('admin/template/assets/images/favicon.png') }}" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.1.5/index.global.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.1.5/index.global.min.js" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.1.5/index.global.min.css"
+        integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.1.5/index.global.min.js" integrity="sha512-..."
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-..." crossorigin="anonymous" />
 </head>
-@auth
-    
-<body>
-  <div class="container-scroller">
-    <!-- partial:partials/_sidebar.html -->
-    @include('partial.sidebar')
-    <!-- partial -->
-    <div class="container-fluid page-body-wrapper">
-      <!-- partial:partials/_navbar.html -->
-      {{-- @include('partial.navbar') --}}
-      <!-- partial -->
-      <div class="main-panel">
-        <div class="content-wrapper">
-          <div class="container">
-            @yield('isi')
-            </div>
-            </div>
-        </div>
-        <!-- page-body-wrapper ends -->
-        </div>
-        <!-- plugins:js -->
-    <script src="{{ asset('admin/template/assets/vendors/js/vendor.bundle.base.js') }}"></script>
-    <!-- Plugin js for this page -->
-    <script src="{{ asset('admin/template/assets/vendors/chart.js/Chart.min.js') }}"></script>
-    <script src="{{ asset('admin/template/assets/vendors/progressbar.js/progressbar.min.js') }}"></script>
-    <script src="{{ asset('admin/template/assets/vendors/owl-carousel-2/owl.carousel.min.js') }}"></script>
-    <!-- inject:js -->
-    <script src="{{ asset('admin/template/assets/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('admin/template/assets/js/hoverable-collapse.js') }}"></script>
-    <script src="{{ asset('admin/template/assets/js/misc.js') }}"></script>
-    <script src="{{ asset('admin/template/assets/js/settings.js') }}"></script>
-    <script src="{{ asset('admin/template/assets/js/todolist.js') }}"></script>
-    <!-- Custom js for this page -->
-    <script src="{{ asset('admin/template/assets/js/dashboard.js') }}"></script>
-    
-    @endauth
 
-    @guest
-{{-- Kalau Guest otomatis masuk /pengguna --}}
-<script>
-  window.location.href = "/pengguna";
-</script>
-@endguest
-    </body>
-    </html>
-    
+{{-- hanya tampil ketika auth --}}
+@auth
+
+    <body>
+        <div class="container-scroller">
+            <!-- partial:partials/_sidebar.html -->
+            @include('partial.sidebar')
+            <!-- partial -->
+            <div class="container-fluid page-body-wrapper">
+                <!-- partial:partials/_navbar.html -->
+                {{-- @include('partial.navbar') --}}
+                <!-- partial -->
+                <div class="main-panel">
+                    <div class="content-wrapper">
+                        <div class="container">
+                            @yield('isi')
+                        </div>
+                    </div>
+                </div>
+                <!-- page-body-wrapper ends -->
+            </div>
+            <!-- plugins:js -->
+            <script src="{{ asset('admin/template/assets/vendors/js/vendor.bundle.base.js') }}"></script>
+            <!-- Plugin js for this page -->
+            <script src="{{ asset('admin/template/assets/vendors/chart.js/Chart.min.js') }}"></script>
+            <script src="{{ asset('admin/template/assets/vendors/progressbar.js/progressbar.min.js') }}"></script>
+            <script src="{{ asset('admin/template/assets/vendors/owl-carousel-2/owl.carousel.min.js') }}"></script>
+            <!-- inject:js -->
+            <script src="{{ asset('admin/template/assets/js/off-canvas.js') }}"></script>
+            <script src="{{ asset('admin/template/assets/js/hoverable-collapse.js') }}"></script>
+            <script src="{{ asset('admin/template/assets/js/misc.js') }}"></script>
+            <script src="{{ asset('admin/template/assets/js/settings.js') }}"></script>
+            <script src="{{ asset('admin/template/assets/js/todolist.js') }}"></script>
+            <!-- Custom js for this page -->
+            <script src="{{ asset('admin/template/assets/js/dashboard.js') }}"></script>
+
+        @endauth
+
+        @guest
+            {{-- Kalau Guest otomatis masuk /pengguna --}}
+            {{-- fungsi sama seperti midleware --}}
+            <script>
+                window.location.href = "/pengguna";
+            </script>
+        @endguest
+</body>
+
+</html>
